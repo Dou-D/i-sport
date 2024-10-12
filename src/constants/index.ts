d6be681467fb971b30d662c03349da2dc7634a56
src <2659279285@qq.com>
2024-10-12 19:31:03 +0800

    feat: 新增打卡内容

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_NAME = 'Umi Max';
 
 export const USER_FORM_KEY = 'UserForm';
+
+export const USER_TOKEN_CARD = 'UserTokenCard';
--- src/pages/Recover/index.tsx
+++ src/pages/Recover/index.tsx
@@ -1,24 +1,43 @@
-import { Col, Row } from 'antd';
-import ReactPlayer from 'react-player';
+import { useState } from 'react';
+import './index.css';
+import {  USER_TOKEN_CARD } from '@/constants';
+import { message } from 'antd';
 
 export default function Recover() {
-  const videoList = [];
-  for (let i = 0; i < 9; i++) {
-    videoList.push(
-      <Col span={7} style={{ margin: '10px' }} >
-        <ReactPlayer
-          url="https://cdn.pixabay.com/video/2016/04/18/2849-163375551_large.mp4" //地址
-          width="100%" //宽度
-          height="100%" //高度
-          playing={false} //是否自动播放
-          controls //控制
-        />
-      </Col>,
-    );
-  }
+  const [isCheckedIn, setIsCheckedIn] = useState(localStorage.getItem(USER_TOKEN_CARD)); // 状态：是否已打卡
+  const [isAnimating, setIsAnimating] = useState(false); // 控制动画效果
+
+
+  const handleCheckIn = () => {
+    // 防止多次点击打卡
+    if (!isAnimating) {
+      setIsAnimating(true);
+
+      if (localStorage.getItem(USER_TOKEN_CARD) === null) {
+        localStorage.setItem(USER_TOKEN_CARD, new Date())
+        message.success('打卡成功哦！')
+        setTimeout(() => {
+          setIsCheckedIn(!isCheckedIn); // 切换打卡状态
+        }, 600); // 动画时长与CSS同步
+      }else {
+        message.warning('今日已打卡')
+      }
+      setIsAnimating(false); // 动画完成后重置
+    }
+  };
+
   return (
-    <>
-      <Row>{videoList}</Row>
-    </>
+    <div className="container">
+      <p>{isCheckedIn ? '已打卡 ✔️' : '尚未打卡 ❌'}</p>
+
+      <div
+        className={`circle-button ${isAnimating ? 'animate' : ''} ${
+          isCheckedIn ? 'checked-in' : ''
+        }`}
+        onClick={handleCheckIn}
+      >
+        {isCheckedIn ? '完成' : '打卡'}
+      </div>
+    </div>
   );
 }
